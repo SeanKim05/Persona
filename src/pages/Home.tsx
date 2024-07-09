f1eaee839b5400296ee93c32188dacf125de72f9
유현 <a@1ui-MacBookAir.local>
2024-07-09 17:55:07 +0900

    feat: 절대경로 설정 및 메인화면 작업 진행

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,44 @@
+import { useEffect, useState } from "react";
+import introImg from "@/assets/introImg.jpg";
+
 export default function Home() {
-  return <div className="h-lvh bg-red-900">Home</div>;
+  const [isBlur, setIsBlur] = useState(true);
+
+  const screenHeight = window.screen.height;
+  console.log(screenHeight);
+
+  const scrollHandler = () => {
+    if (window.scrollY > 400) {
+      setIsBlur(false);
+    } else {
+      setIsBlur(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
+  return (
+    <div className="bg-black">
+      <div className="h-[700px]"></div>
+      <div className={`relative ${isBlur ? "blur-lg" : "blur-0"}`}>
+        <img
+          src={introImg}
+          alt="Blurry"
+          className={`w-full h-screen`}
+          style={{
+            transition: "filter 1s ease-in-out",
+          }}
+        />
+        <h1 className="absolute top-0 left-0  text-white text-4xl font-bold">
+          Persona
+        </h1>
+      </div>
+    </div>
+  );
 }
