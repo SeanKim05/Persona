ef7c618bbcca57d01db5dd6af2fcbf9218dc304e
SeanKim05 <dbgus20609@gmail.com>
2024-07-19 17:35:51 +0900

    feat: 색상추가 및 이미지 이펙트

--- src/pages/Me.tsx
+++ src/pages/Me.tsx
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Me = () => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const [flippedIndices, setFlippedIndices] = useState<number[]>([]);
 
   const onWheel = (e: React.WheelEvent<HTMLDivElement>) => {
     if (scrollContainerRef.current) {
@@ -9,23 +10,34 @@ const Me = () => {
     }
   };
 
+  const toggleFlip = (index: number) => {
+    setFlippedIndices((prevFlippedIndices) =>
+      prevFlippedIndices.includes(index)
+        ? prevFlippedIndices.filter((i) => i !== index)
+        : [...prevFlippedIndices, index]
+    );
+  };
+
   const items = [
     {
-      className: "bg-black",
+      className: "bg-[#333D51]",
       imageSrc: new URL(`../assets/img/me/me0.jpg`, import.meta.url).href,
-      // text: "와 내용을 적어야 하는데 디자인이 정말 없어 보인다 와 내용을 적어야 하는데<br />와 내용을 적어야 하는데 디자인이 정말 없어 보인다 와 내용을 적어야 하는데",
+      text: "abc",
     },
     {
       className: "bg-[#CBD0D8]",
       imageSrc: new URL(`../assets/img/me/me1.jpg`, import.meta.url).href,
+      text: "abc",
     },
     {
       className: "bg-[#F4F4EA] text-black",
       imageSrc: new URL(`../assets/img/me/me2.jpg`, import.meta.url).href,
+      text: "abc",
     },
     {
       className: "bg-[#D3AC2B] text-black",
       imageSrc: new URL(`../assets/img/me/me3.jpg`, import.meta.url).href,
+      text: "abc",
     },
   ];
 
@@ -38,19 +50,25 @@ const Me = () => {
       {items.map((item, index) => (
         <div
           key={index}
-          className={`flex min-w-[800px] h-full ${item.className} justify-center items-center`}
+          onClick={() => toggleFlip(index)}
+          className={`flex min-w-[800px] h-full justify-center items-center flip-card ${
+            item.className
+          } ${flippedIndices.includes(index) ? "flipped" : ""}`}
         >
-          <div className="relative">
-            <img
-              className="flexRowCenter max-h-screen px-[10%]"
-              src={item.imageSrc}
-            />
-            <h2
-              style={{ fontFamily: "mj" }}
-              className="absolute top-[-30px] flex justify-center w-full text-[50px]"
-            >
-              Photography
-            </h2>
+          <div className="flip-card-inner">
+            <div className="flip-card-front flexRowCenter">
+              <img
+                className=" flexRowCenter max-h-[90%] px-[10%]"
+                src={item.imageSrc}
+              />
+              <h2
+                style={{ fontFamily: "mj" }}
+                className="absolute top-[10%] flex justify-center w-full text-[50px]"
+              >
+                Photography
+              </h2>
+            </div>
+            <div className={`flip-card-back bg-black`}>{item.text}</div>
           </div>
         </div>
       ))}
